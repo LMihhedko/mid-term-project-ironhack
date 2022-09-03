3a1448d5e8c4cb4a83c08d1a9834aa9253b85c5f
Mihhedkol <mihhdekol@gmail.com>
2022-09-03 16:12:19 +0200

    project and contact section done + mobile

--- js/index.js
+++ js/index.js
@@ -1,27 +1,19 @@
-window.onload = function () {
-  let menu = false;
-  //activar listener para menu hamburguesa
+window.addEventListener("load", function () {
   document.getElementById("menu").addEventListener("click", showHideNav);
-  //detectar cuando se redimensiona la pantalla
-  window.onresize = hide;
-};
-
-function hide() {
-  //ocultar el menu cuando la resolución sea superior a 576px
-  if (window.innerWidth >= 576) {
-    document.querySelector("#navbar-links").style.display = "flex";
-    menu = false;
+  const btns = document.querySelectorAll(".header-button");
+  for (const btn of btns) {
+    btn.addEventListener("click", function () {
+      location.href = "../contact.html";
+    });
   }
-}
+});
 
 function showHideNav() {
-  //si el menu está oculto lo muestra
-  if (menu == false) {
-    document.querySelector("#navbar-links").style.display = "none";
-    menu = true;
+  const navMenu = document.querySelector("#navbar-menu");
+
+  if (navMenu.style.display == "none") {
+    navMenu.style.display = "flex";
   } else {
-    //si está visible lo oculta
-    document.querySelector("#navbar-links").style.display = "flex";
-    menu = false;
+    navMenu.style.display = "none";
   }
 }
