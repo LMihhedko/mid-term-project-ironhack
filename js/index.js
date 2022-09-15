371cbe13040010fcb8a12379a8ea4f55a7257943
Mihhedkol <mihhdekol@gmail.com>
2022-09-15 20:06:38 +0200

    fix form error messages

--- js/index.js
+++ js/index.js
@@ -53,59 +53,66 @@ window.addEventListener("load", function () {
   }
 
   function submitForm() {
+    document.getElementById("phone-error").innerHTML = " ";
+    document.getElementById("message-error").innerHTML = " ";
+
     let phone = document.getElementById("phone").value;
     let message = document.getElementById("message").value;
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
 
+    const form = document.getElementById("form");
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
     if (!phone.startsWith("+")) {
       document.getElementById("phone-error").innerHTML =
         "Number must start with +";
-    } else if (message.length < 50) {
+      return false;
+    }
+
+    if (message.length < 50) {
       document.getElementById("message-error").innerHTML =
         "Minimum length of the message is 50 characters";
-    } else {
-      const form = document.getElementById("form");
-
-      form.addEventListener("submit", (e) => {
-        e.preventDefault();
-      });
-
-      const url = "https://database.deta.sh/v1/a0wwnrex/contactmessages/items";
-
-      const data = {
-        name: name,
-        email: email,
-        phone: phone,
-        message: message,
-      };
-
-      const body = { item: data };
-
-      const params = {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "X-API-Key": "a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H",
-        },
-        body: JSON.stringify(body),
-      };
-
-      fetch(url, params)
-        .then((response) => {
-          if (response.ok) return response.json();
-        })
-        .then((json) => {
-          console.log(json);
-          document.getElementById("message-error").innerHTML =
-            "Enviado correctamente";
-          document.getElementById("phone").value = "";
-          document.getElementById("message").value = "";
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-        })
-        .catch((err) => console.log(err));
+      return false;
     }
+
+    const url = "https://database.deta.sh/v1/a0wwnrex/contactmessages/items";
+
+    const data = {
+      name: name,
+      email: email,
+      phone: phone,
+      message: message,
+    };
+
+    const body = { item: data };
+
+    const params = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-API-Key": "a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H",
+      },
+      body: JSON.stringify(body),
+    };
+
+    fetch(url, params)
+      .then((response) => {
+        if (response.ok) return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        document.getElementById("message-error").innerHTML =
+          "Enviado correctamente";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+      })
+      .catch((err) => console.log(err));
   }
 });
